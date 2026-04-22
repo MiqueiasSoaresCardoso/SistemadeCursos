36ca66db1ac780e814aab4fd983d2247a0ea9ada
MiqueiasSoaresCardoso <miqueiasccccc@gmail.com>
2026-04-22 10:23:56 -0300

    Matricula

---
+++ src/main/java/br/edu/cursosifpb/model/Matricula.java
@@ -0,0 +1,83 @@
+package br.edu.cursosifpb.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Matricula {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "aluno_id")
+    private Aluno aluno;
+
+    @ManyToOne
+    @JoinColumn(name = "curso_id")
+    private Curso curso;
+
+    private Double nota1;
+    private Double nota2;
+    private Double media;
+
+    public Matricula() {
+    }
+
+    public Matricula(Long id, Aluno aluno, Curso curso, Double nota1, Double nota2, Double media) {
+        this.id = id;
+        this.aluno = aluno;
+        this.curso = curso;
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+        this.media = media;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Aluno getAluno() {
+        return aluno;
+    }
+
+    public void setAluno(Aluno aluno) {
+        this.aluno = aluno;
+    }
+
+    public Curso getCurso() {
+        return curso;
+    }
+
+    public void setCurso(Curso curso) {
+        this.curso = curso;
+    }
+
+    public Double getNota1() {
+        return nota1;
+    }
+
+    public void setNota1(Double nota1) {
+        this.nota1 = nota1;
+    }
+
+    public Double getNota2() {
+        return nota2;
+    }
+
+    public void setNota2(Double nota2) {
+        this.nota2 = nota2;
+    }
+
+    public Double getMedia() {
+        return media;
+    }
+
+    public void setMedia(Double media) {
+        this.media = media;
+    }
+}
