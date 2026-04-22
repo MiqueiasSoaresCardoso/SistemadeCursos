1bd37065a41f52c0a05edb3ecf351df2f4ec57d9
MiqueiasSoaresCardoso <miqueiasccccc@gmail.com>
2026-04-22 08:45:56 -0300

    criando o model

---
+++ src/main/java/br/edu/cursosifpb/CursosifpbApplication.java
@@ -0,0 +1,13 @@
+package br.edu.cursosifpb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursosifpbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursosifpbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/cursosifpb/CursosifpbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.cursosifpb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursosifpbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
