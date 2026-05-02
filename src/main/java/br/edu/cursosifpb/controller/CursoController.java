9d4bbc40c49b8659e7f228a2e964149a649780c2
MiqueiasSoaresCardoso <miqueiasccccc@gmail.com>
2026-05-02 12:07:25 -0300

    correções

--- src/main/java/br/edu/cursosifpb/controller/CursoController.java
+++ src/main/java/br/edu/cursosifpb/controller/CursoController.java
@@ -30,14 +30,29 @@ public class CursoController {
     @DeleteMapping("/remover/{id}")
     //Remover Curso
     public void deleteCurso(@PathVariable long id){
+        /*faz uma leitura na lista de cursos e verifica se
+        algum possui o mesmo id passado na URL, se possuir ele remove o curso
+         */
+        cursos.removeIf(curso -> curso.getId() == id);
+    }
+
+    @DeleteMapping("/remover")
+    //Remover Curso
+    public void deleteCursoversao02(@RequestParam long id){
+         /*faz uma leitura na lista de cursos e verifica se
+        algum possui o mesmo id passado na URL, se possuir ele remove o curso
+         */
         cursos.removeIf(curso -> curso.getId() == id);
     }
 
     @PutMapping("/editar/{id}")
     //Editar Curso
     public void editarCurso(@PathVariable long id, @RequestBody Curso cursoatualizado){
+        //Faz um for na lista de cursos existentes
         for (Curso c : cursos){
+            //verifica se existe algum curso na lista com o mesmo id passado na URL(caminho)
             if(c.getId() == id){
+                //se existir ele atualiza os valore com base nos metódos set
                 c.setNome(cursoatualizado.getNome());
                 c.setCargaHoraria(cursoatualizado.getCargaHoraria());
                 break;
